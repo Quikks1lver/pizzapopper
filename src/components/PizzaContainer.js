f1f0c715f4c0fb9e45a3129c60cda66abeea8da8
Adam Fernandes <fernadam8@gmail.com>
2020-04-29 15:12:25 -0400

    used useEffect() hook to correct timing problems!

--- src/components/PizzaContainer.js
+++ src/components/PizzaContainer.js
@@ -1,36 +1,42 @@
 import React from "react";
+import { useEffect } from "react";
 import { makePizza } from "../redux/index";
 import { connect, useDispatch } from "react-redux";
 import Pizza from "../pictures/pizza.png";
 import "../App.css";
 
 const TIME_TARGET = 1000;
+var louieIntervals = [];
+var planetIntervals = [];
 
 function louieMakesPizzas({ numLouies, makePizza }) {
   if (numLouies > 0) {
+    louieIntervals.forEach(clearTimeout);
     const time = TIME_TARGET - 1 * numLouies;
-    return setTimeout(() => makePizza(), time);
+    const repeat1 = setInterval(() => makePizza(), time);
+    louieIntervals.push(repeat1);
   }
 }
 
 function rocketMakesTrips({ numPlanets, makePizza }) {
   if (numPlanets > 0) {
+    planetIntervals.forEach(clearTimeout);
     const time = TIME_TARGET - 5 * numPlanets;
-    return setTimeout(() => makePizza(), time);
+    const repeat2 = setInterval(() => makePizza(), time);
+    planetIntervals.push(repeat2);
   }
 }
 
 function PizzaContainer(props) {
   const dispatch = useDispatch();
 
+  useEffect(() => louieMakesPizzas(props), [props.numLouies]);
+  useEffect(() => rocketMakesTrips(props), [props.numPlanets]);
+
   return (
     <div>
       <p>
         <strong>pizzas popped: 🍕{props.numPizzas}🍕</strong>
-      </p>
-      <p>
-        {props.numLouies > 0 ? louieMakesPizzas(props) : false}
-        {props.numPlanets > 0 ? rocketMakesTrips(props) : false}
       </p>
       <button onClick={() => dispatch(makePizza())}>
         <img className="img" src={Pizza} />
--- src/thresholds.js
+++ src/thresholds.js
@@ -3,7 +3,7 @@ export const LOUIE_CAP = 1000;
 export const LOUIE_COST = 3;
 
 export const PLANET_THRESHOLD = 50;
-export const PLANET_CAP = 200;
+export const PLANET_CAP = 100;
 export const PLANET_COST = 50;
 
 export const TARGET_SCORE = 10000;
