ba73773164bb02ee825df47d6674a55a70fb30ef
Adam Fernandes <fernadam8@gmail.com>
2020-08-17 10:16:55 -0400

    added a comment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Provider store={store}>
       <div>
         <Header />
-        <EverythingContainer />
+        <EverythingContainer /> {/*testing*/}
         <Footer />
       </div>
     </Provider>
