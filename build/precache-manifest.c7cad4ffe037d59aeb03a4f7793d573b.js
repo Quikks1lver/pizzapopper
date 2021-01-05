00f89a1a98699f4438a70d1d970eb8643c94c7b1
Adam Fernandes <fernadam8@gmail.com>
2021-01-05 17:03:18 -0500

    build

---
+++ build/precache-manifest.c7cad4ffe037d59aeb03a4f7793d573b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b684e19214e4e5a29b1c64f570df93a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37dbccd278b7ab5fe918",
+    "url": "/static/css/main.4242cb94.chunk.css"
+  },
+  {
+    "revision": "6c5ec64b8c6a3002b74d",
+    "url": "/static/js/2.e3e5de21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e3e5de21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37dbccd278b7ab5fe918",
+    "url": "/static/js/main.c0dab647.chunk.js"
+  },
+  {
+    "revision": "f7807d8f9171af0e3843",
+    "url": "/static/js/runtime-main.a77ce5cb.js"
+  },
+  {
+    "revision": "ec914dc04d6c08876f995175f21dfdee",
+    "url": "/static/media/pizza.ec914dc0.png"
+  },
+  {
+    "revision": "32d1f802c0166f0aea1d0a1ec19e339f",
+    "url": "/static/media/planet.32d1f802.jpg"
+  },
+  {
+    "revision": "b3c29ab804af844c1241872a2e71f4e2",
+    "url": "/static/media/wizard.b3c29ab8.jpeg"
+  }
+]);
